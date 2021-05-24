3a2cb8575d6cfeb3cebb344f00a95d4f43583ef6
CyberFlaw <53346567+CyberFlaw@users.noreply.github.com>
2021-05-24 19:52:52 +0530

    initial commit

---
+++ pages/covid.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function covid() {
+  return (
+    <div>
+      <h1>Helo Covid</h1>
+    </div>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Covid Focus</title>
+        <meta name="description" content="A panchayat covid focus website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>{/* Main */}</main>
+
+      <footer>{/* Footer */}</footer>
+    </div>
+  );
+}
