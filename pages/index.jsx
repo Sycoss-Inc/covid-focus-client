e7eb7759b2a4473b56a0e3f62eaa40b1257bd631
navneethvariar14 <57003649+navneethvariar14@users.noreply.github.com>
2021-06-09 20:02:36 +0530

    Notices component recreated

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import Rate from "../components/Rate";
 import Notice from "../components/Notice";
 
 import Link from "next/link";
+import Noticetrail from "../components/noticetrail";
 
 export default function Home() {
   return (
@@ -26,19 +27,25 @@ export default function Home() {
           <Notice />
           <div className={`row ${styles.stats}`}>
             {" "}
-            <div className= {`col-12 col-md-7 ${styles.statQues}`}>
-             <div> <p>എന്റെ പഞ്ചായത്തിൽ എത്ര കൊറോണ രോഗികളുണ്ട് ?</p>
-            
-              <p className="d-none d-md-block">എന്റെ പഞ്ചായത്തിൽ ഇന്ന് എത്ര പുതിയ കൊറോണ രോഗികളുണ്ട് ?</p>
-              <p className="d-none d-md-block">എന്റെ പഞ്ചായത്തിൽ എത്രപേർക്ക് രോഗം ഭേദമായി ?</p>
-                <p >എന്റെ പഞ്ചായത്തിലെ പോസിറ്റീവ് നിരക്ക് എത്രയാണ്?</p>
-                <Link href="/stats"><button className="btn ">VIEW FULL STATISTICS</button></Link>
-            </div></div>
-            <div className={` col-12 col-md-5 ${styles.statsImg}`}>
-             
+            <div className={`col-12 col-md-7 ${styles.statQues}`}>
+              <div>
+                {" "}
+                <p>എന്റെ പഞ്ചായത്തിൽ എത്ര കൊറോണ രോഗികളുണ്ട് ?</p>
+                <p className="d-none d-md-block">
+                  എന്റെ പഞ്ചായത്തിൽ ഇന്ന് എത്ര പുതിയ കൊറോണ രോഗികളുണ്ട് ?
+                </p>
+                <p className="d-none d-md-block">
+                  എന്റെ പഞ്ചായത്തിൽ എത്രപേർക്ക് രോഗം ഭേദമായി ?
+                </p>
+                <p>എന്റെ പഞ്ചായത്തിലെ പോസിറ്റീവ് നിരക്ക് എത്രയാണ്?</p>
+                <Link href="/stats">
+                  <button className="btn ">VIEW FULL STATISTICS</button>
+                </Link>
+              </div>
             </div>
+            <div className={` col-12 col-md-5 ${styles.statsImg}`}></div>
           </div>
-          
+
           <Pass />
         </main>
       </div>
